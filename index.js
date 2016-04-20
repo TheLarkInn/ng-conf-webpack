63b91cf2cd46bc30d99420472e0a363540bf08f7
Sean Larkin <Sean.Larkin@mutualofomaha.com>
2016-04-20 15:02:57 -0500

    HI

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var HelloWorldComponent = require('./src/HelloWorldComponent.js');
+var ButtonComponent = require('./src/ButtonComponent.js');
 
 document.body.appendChild(HelloWorldComponent());
-
+document.body.appendChild(ButtonComponent());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 // Simplest webpack example known to human kind.
 
 module.exports = {
+  progress: true,
 	entry: './index.js',
 	output:  {
 		filename: 'bundle.js',
 		path: 'dist/'
-	}
+	}, 
+  devServer: {
+    contentBase: 'dist/'
+  }
 }
