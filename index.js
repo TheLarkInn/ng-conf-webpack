4ef7d6cd8587cde6ca90ca28ace6d88f3b5482e9
Sean Larkin <Sean.Larkin@mutualofomaha.com>
2016-04-20 14:09:34 -0500

    Reverted to step one of the learning. Process.

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 var HelloWorldComponent = require('./src/HelloWorldComponent.js');
-var ButtonComponent = require('./src/ButtonComponent.js');
-var ES6StyleInputComponent = require('./src/ES6StyleInputComponent.js'); // Webpack automatically knows imports will work.
-var input = new ES6StyleInputComponent.InputComponent;
 
 document.body.appendChild(HelloWorldComponent());
-document.body.appendChild(ButtonComponent());
 
-document.body.appendChild(input.render());
